eebaa2f39ebde4bfa4d80e69d3b8c68e342ad76c
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-02-23 18:19:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require 'rack'
 class Application
   def call(env)
@@ -11,20 +10,4 @@ class Application
     end
     resp.finish
   end
-=======
-require 'rack'
-class Application
-
-    def call(env)
-        resp = Rack::Response.new
-        time = Time.now.hour
-        if time < 12
-            resp.write "Good Morning!"
-        else
-            resp.write "Good Afternoon!"
-        end
-        resp.finish
-    end
-
->>>>>>> 6dc281f7ecaffb2420f5c814c23b63060015f2e5
 end
